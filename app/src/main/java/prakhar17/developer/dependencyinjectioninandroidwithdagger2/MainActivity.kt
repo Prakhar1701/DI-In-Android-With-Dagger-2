cb763872dcb2c38ad3b1335b9dc65df6fa56514a
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-05-27 20:30:00 +0530

    Initial commit

---
+++ app/src/main/java/prakhar17/developer/dependencyinjectioninandroidwithdagger2/MainActivity.kt
@@ -0,0 +1,38 @@
+package prakhar17.developer.dependencyinjectioninandroidwithdagger2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import prakhar17.developer.dependencyinjectioninandroidwithdagger2.ui.theme.DependencyInjectionInAndroidWithDagger2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DependencyInjectionInAndroidWithDagger2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Dependency Injection In Android With Dagger 2")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        modifier = modifier
+    )
+}
