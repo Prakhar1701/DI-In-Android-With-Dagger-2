ee93385c47e06ed4bd8aabad0d579e77f6d6e7d2
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-05-28 01:02:37 +0530

    Using Singletons
    Singleton allows to have one instance of an object running across the whole application,
    and every time it is provided to a different class, the same instance of that object is injected.

--- app/src/main/java/prakhar17/developer/dependencyinjectioninandroidwithdagger2/SmartPhone.kt
+++ app/src/main/java/prakhar17/developer/dependencyinjectioninandroidwithdagger2/SmartPhone.kt
@@ -2,7 +2,9 @@ package prakhar17.developer.dependencyinjectioninandroidwithdagger2
 
 import android.util.Log
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class SmartPhone @Inject constructor(
     private val battery: Battery,
     private val memoryCard: MemoryCard,
--- app/src/main/java/prakhar17/developer/dependencyinjectioninandroidwithdagger2/SmartPhoneComponent.kt
+++ app/src/main/java/prakhar17/developer/dependencyinjectioninandroidwithdagger2/SmartPhoneComponent.kt
@@ -1,7 +1,9 @@
 package prakhar17.developer.dependencyinjectioninandroidwithdagger2
 
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
     /*The name of the function can be anything but
