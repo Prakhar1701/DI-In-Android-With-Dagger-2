db72f24dec018bd4604bc9325a7788c73fc39a07
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-05-27 23:34:58 +0530

    Converted Battery class into an interface

--- app/src/main/java/prakhar17/developer/dependencyinjectioninandroidwithdagger2/Battery.kt
+++ app/src/main/java/prakhar17/developer/dependencyinjectioninandroidwithdagger2/Battery.kt
@@ -3,11 +3,7 @@ package prakhar17.developer.dependencyinjectioninandroidwithdagger2
 import android.util.Log
 import javax.inject.Inject
 
-class Battery @Inject constructor(){
-    init {
-        Log.i("STATUS", "Battery Constructed")
-    }
-
+interface Battery {
     fun getPower() {
         Log.i("STATUS", "Battery Power Is Available")
     }
